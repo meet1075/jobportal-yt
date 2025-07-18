af5b3e25b8d613dd89fd183a38562e919f445708
meet1075 <meetsoni1075@gmail.com>
2025-07-18 19:09:52 +0530

    refactor: update secret key references and improve category carousel component

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -11,33 +11,38 @@ const category = [
     "Data Science",
     "Graphic Designer",
     "FullStack Developer"
-]
+];
 
 const CategoryCarousel = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
     const searchJobHandler = (query) => {
         dispatch(setSearchedQuery(query));
         navigate("/browse");
-    }
+    };
 
     return (
         <div>
             <Carousel className="w-full max-w-xl mx-auto my-20">
                 <CarouselContent>
-                    {
-                        category.map((cat, index) => (
-                            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
-                                <Button onClick={()=>searchJobHandler(cat)} variant="outline" className="rounded-full">{cat}</Button>
-                            </CarouselItem>
-                        ))
-                    }
+                    {category.map((cat) => (
+                        <CarouselItem key={cat} className="md:basis-1/2 lg-basis-1/3">
+                            <Button
+                                onClick={() => searchJobHandler(cat)}
+                                variant="outline"
+                                className="rounded-full"
+                            >
+                                {cat}
+                            </Button>
+                        </CarouselItem>
+                    ))}
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
             </Carousel>
         </div>
-    )
-}
+    );
+};
 
-export default CategoryCarousel
+export default CategoryCarousel;
